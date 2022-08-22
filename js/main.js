77b690e53d19816a90c28116cc876535d20f7015
Abdullah-Alsuedani <abdullah.xvx.2006@gmail.com>
2022-08-22 16:17:17 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -12,16 +12,18 @@ let load = document.createElement("div")
 load.classList.add("load")
 let content;
 let activePage;
+let localPage;
 
 let footerNav = document.querySelectorAll(".footer .links li")
 
-    page.innerHTML = ""
+if(!window.localStorage.getItem("localPage")){
+        page.innerHTML = ""
     loadCont.appendChild(load)
     page.appendChild(loadCont)
     footerNav.forEach(ele=>ele.classList.remove("active"))
 
     setTimeout(() => {
-        let activePage = new XMLHttpRequest();
+        activePage = new XMLHttpRequest();
         activePage.open("GET", "home/home.html")
         activePage.onreadystatechange = function(){
             page.innerHTML = activePage.responseText
@@ -32,6 +34,8 @@ let footerNav = document.querySelectorAll(".footer .links li")
         activePage.send()
         footerNav[0].classList.add("active")
     }, 700);
+}
+
 
 function navs(navlies){
 navlies.forEach((ele) =>{
@@ -39,6 +43,16 @@ navlies.forEach((ele) =>{
         navlies.forEach((ele)=>{
             ele.classList.remove("active")
         })
+        navLis.forEach((ele)=>{
+            ele.classList.remove("active")
+        })
+        if(e.currentTarget.textContent === "Home"){
+            navLis[0].classList.add("active")
+        }else if(e.currentTarget.textContent === "About Us"){
+            navLis[1].classList.add("active")
+        }else if(e.currentTarget.textContent === "Projects"){
+            navLis[2].classList.add("active")
+        }
         e.currentTarget.classList.add("active")
         if(e.currentTarget.textContent === "About Us" && navBack.style.left == "0px"){
             navBack.style.left = "110px"
@@ -66,7 +80,7 @@ navlies.forEach((ele) =>{
             page.appendChild(loadCont)
             footerNav.forEach(ele=>ele.classList.remove("active"))
             setTimeout(() => {
-                let activePage = new XMLHttpRequest();
+                activePage = new XMLHttpRequest();
                 activePage.open("GET", "home/home.html")
                 activePage.onreadystatechange = function(){
                     page.innerHTML = activePage.responseText
@@ -76,6 +90,8 @@ navlies.forEach((ele) =>{
                 }
                 activePage.send()
                 footerNav[0].classList.add("active")
+                localPage = "Home";
+                window.localStorage.setItem("localPage", localPage)
             }, 700);
         }
         if(e.currentTarget.textContent === "About Us"){
@@ -84,16 +100,15 @@ navlies.forEach((ele) =>{
             page.appendChild(loadCont)
             footerNav.forEach(ele=>ele.classList.remove("active"))
             setTimeout(() => {
-                let activePage = new XMLHttpRequest();
+                activePage = new XMLHttpRequest();
                 activePage.open("GET", "about/about.html")
                 activePage.onreadystatechange = function(){
                     page.innerHTML = activePage.responseText
-                    if(activePage.readyState === 4){
-                        aboutEvents()
-                    }
                 }
                 activePage.send()
                 footerNav[1].classList.add("active")
+                localPage = "About Us";
+                window.localStorage.setItem("localPage", localPage)
             }, 700);
         }
         if(e.currentTarget.textContent === "Projects"){
@@ -102,7 +117,7 @@ navlies.forEach((ele) =>{
             page.appendChild(loadCont)
             footerNav.forEach(ele=>ele.classList.remove("active"))
             setTimeout(() => {
-                let activePage = new XMLHttpRequest();
+                activePage = new XMLHttpRequest();
                 activePage.open("GET", "projects/projects.html")
                 activePage.onreadystatechange = function(){
                     page.innerHTML = activePage.responseText
@@ -112,10 +127,80 @@ navlies.forEach((ele) =>{
                 }
                 activePage.send()
                 footerNav[2].classList.add("active")
+                localPage = "Projects";
+                window.localStorage.setItem("localPage", localPage)
             }, 700);
         }
+        
     })
 })
+}
+
+window.onload = function(){
+    if(window.localStorage.getItem("localPage")){
+        navLis.forEach((ele)=>{
+            ele.classList.remove("active")
+        })
+        if(window.localStorage.getItem("localPage") === "Home"){
+            page.innerHTML = ""
+            loadCont.appendChild(load)
+            page.appendChild(loadCont)
+            footerNav.forEach(ele=>ele.classList.remove("active"))
+            setTimeout(() => {
+                activePage = new XMLHttpRequest();
+                activePage.open("GET", "home/home.html")
+                activePage.onreadystatechange = function(){
+                    page.innerHTML = activePage.responseText
+                    if(activePage.readyState === 4){
+                        homeEvents()
+                    }
+                }
+                activePage.send()
+                footerNav[0].classList.add("active")
+                localPage = "Home";
+                navLis[0].classList.add("active")
+            }, 700);
+        }else if(window.localStorage.getItem("localPage") === "About Us"){
+            page.innerHTML = ""
+            loadCont.appendChild(load)
+            page.appendChild(loadCont)
+            footerNav.forEach(ele=>ele.classList.remove("active"))
+            setTimeout(() => {
+                activePage = new XMLHttpRequest();
+                activePage.open("GET", "about/about.html")
+                activePage.onreadystatechange = function(){
+                    page.innerHTML = activePage.responseText
+                }
+                activePage.send()
+                footerNav[1].classList.add("active")
+                localPage = "About Us";
+                window.localStorage.setItem("localPage", localPage)
+                navBack.style.left = "110px"
+                navLis[1].classList.add("active")
+            }, 700);
+        }else if(window.localStorage.getItem("localPage") === "Projects"){
+            page.innerHTML = ""
+            loadCont.appendChild(load)
+            page.appendChild(loadCont)
+            footerNav.forEach(ele=>ele.classList.remove("active"))
+            setTimeout(() => {
+                activePage = new XMLHttpRequest();
+                activePage.open("GET", "projects/projects.html")
+                activePage.onreadystatechange = function(){
+                    page.innerHTML = activePage.responseText
+                    if(activePage.readyState === 4){
+                        projectsEvents()
+                    }
+                }
+                activePage.send()
+                footerNav[2].classList.add("active")
+                localPage = "Projects";
+                window.localStorage.setItem("localPage", localPage)
+                navBack.style.left = "235px"
+                navLis[2].classList.add("active")
+            }, 700);
+        } 
+    }
 }
 
 navs(navLis)
@@ -169,14 +254,13 @@ function homeEvents(){
 
         let showProjects = document.querySelector(".last-proj .show-btn")
         showProjects.onclick = function(){
-            navBack.style.left = "235px";
-            navLis.forEach(ele=>ele.classList.remove("active"))
-            Array.from(navLis)[2].classList.add("active")
             page.innerHTML = ""
             loadCont.appendChild(load)
             page.appendChild(loadCont)
+            footerNav.forEach(ele=>ele.classList.remove("active"))
+            navLis.forEach(ele=>ele.classList.remove("active"))
             setTimeout(() => {
-                let activePage = new XMLHttpRequest();
+                activePage = new XMLHttpRequest();
                 activePage.open("GET", "projects/projects.html")
                 activePage.onreadystatechange = function(){
                     page.innerHTML = activePage.responseText
@@ -185,8 +269,46 @@ function homeEvents(){
                     }
                 }
                 activePage.send()
+                footerNav[2].classList.add("active")
+                localPage = "Projects";
+                window.localStorage.setItem("localPage", localPage)
+                navBack.style.left = "235px"
+                navLis[2].classList.add("active")
             }, 700);
         }
+}
+
+function projectsEvents(){
+    let filter = document.querySelectorAll(".projects-sec .filter p")
+    let cards = document.querySelectorAll(".projects-sec .container .card");
+    cards.forEach((ele)=>{
+        filter.forEach((filt) =>{
+            filt.addEventListener("click", function(e){
+                cards.forEach(ele=>ele.classList.remove("show"))
+                setTimeout(() => {
+                    if(e.target.textContent === "All Projects"){
+                            ele.classList.add("show")
+                            filter.forEach(ele=>ele.classList.remove("active")) 
+                            e.target.classList.add("active")
+                    }
+                    if(e.target.textContent === "Last Maded Projects"){
+                        if(ele.title.split(" ").includes("last")){
+                            ele.classList.add("show")
+                            filter.forEach(ele=>ele.classList.remove("active")) 
+                            e.target.classList.add("active")
+                        }
+                    }
+                    if(e.target.textContent === "Favourite Projects"){
+                        if(ele.title.split(" ").includes("favourite")){
+                            ele.classList.add("show")
+                            filter.forEach(ele=>ele.classList.remove("active")) 
+                            e.target.classList.add("active")
+                        }
+                    }
+                }, 200);
+            })
+        })
+    })
 }
 
 
